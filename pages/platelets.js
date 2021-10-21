1487a71e882882dbbfecb6e6e32644aab7140774
Philip <PhilipBDev@gmail.com>
2021-10-20 23:07:06 -0400

    Update platelets.js

--- pages/platelets.js
+++ pages/platelets.js
@@ -19,16 +19,7 @@ const plasma = () => {
             </div>
             <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
               <p class="leading-relaxed text-lg mb-4">
-                Meggings portland fingerstache lyft, post-ironic fixie man bun
-                banh mi umami everyday carry hexagon locavore direct trade art
-                party. Locavore small batch listicle gastropub farm-to-table
-                lumbersexual salvia messenger bag. Coloring book flannel
-                truffaut craft beer drinking vinegar sartorial, disrupt fashion
-                axe normcore meh butcher. Portland 90's scenester vexillologist
-                forage post-ironic asymmetrical, chartreuse disrupt butcher
-                paleo intelligentsia pabst before they sold out four loko. 3
-                wolf moon brooklyn.
-              </p>
+                 Platelets are small cells in your blood used for a multitude of reasons. They can be used to form clots to stop bleeding, fight cancer, and dealing with serious injuries. Platelets can be donated every 7 days, which is necessary if you can sinced they have to be used with 5 days from the time you donate! The only downside for donating only platelets is that it can take a bit of time, about 2 to 3 hours of your day, which is another reason why platelets are always in demand and why blood banks typically offer more incentives for platelet donations over blood donations.
               <a class="text-indigo-500 inline-flex items-center">
                 Learn More
                 <svg
