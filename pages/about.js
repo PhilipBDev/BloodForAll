c0009d3e4c2ba4419d7a05ccfe8fa24038cecb0a
Philip <philipbdev@gmail.com>
2021-10-25 22:16:02 -0400

    Adjusted images

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+
 const about = () => {
   return (
     <>
       <div className="pt-12 flex justify-center items-center text-center pl-16 pr-16">
         <p>
-           BloodForAll is the Internet's number one resource for information on blood banks and the donation process. We'll teach you what type of donation, from blood to platelets to plasma, would be most beneficial to others depending on your blood type. For example, O- blood is best for blood donations while B+ should focus on donating platelets. With that said, no matter your type, donating blood, plasma, or platelets will help save a life!
+          BloodForAll is the Internet's number one resource for information on
+          blood banks and the donation process. We'll teach you what type of
+          donation, from blood to platelets to plasma, would be most beneficial
+          to others depending on your blood type. For example, O- blood is best
+          for blood donations while B+ should focus on donating platelets. With
+          that said, no matter your type, donating blood, plasma, or platelets
+          will help save a life!
         </p>
       </div>
       <section class="text-gray-600 body-font">
@@ -13,17 +21,21 @@ const about = () => {
               Our Team
             </h1>
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Meet the team responsible for sharing information about the donation process through their experiences!
+              Meet the team responsible for sharing information about the
+              donation process through their experiences!
             </p>
           </div>
           <div class="flex flex-wrap text-center -m-2">
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=14"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/1.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
                     Joel Winger
@@ -34,11 +46,14 @@ const about = () => {
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=4"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/2.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
                     Abed Pudi
@@ -49,11 +64,14 @@ const about = () => {
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=5"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/3.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
                     Annie Brie
@@ -64,24 +82,32 @@ const about = () => {
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=6"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/4.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
-                  <h2 class="text-gray-900 title-font font-medium">Britta Jacobs</h2>
+                  <h2 class="text-gray-900 title-font font-medium">
+                    Britta Jacobs
+                  </h2>
                   <p class="text-gray-500">Programmer</p>
                 </div>
               </div>
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=7"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/5.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
                     Dean Chang
@@ -92,11 +118,14 @@ const about = () => {
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=23"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/6.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
                     Bernie Bernz
@@ -107,11 +136,14 @@ const about = () => {
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=9"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/7.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
                     Atticus Finch
@@ -122,11 +154,14 @@ const about = () => {
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=15"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/8.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
                     Matthew McCree
@@ -137,14 +172,17 @@ const about = () => {
             </div>
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img
+                <Image
                   alt="team"
-                  class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://i.pravatar.cc/100?u=11"
-                ></img>
+                  class="bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="/avatars/9.jpg"
+                  priority={true}
+                  width={64}
+                  height={64}
+                ></Image>
                 <div class="flex-grow">
                   <h2 class="text-gray-900 title-font font-medium">
-                    Ford Willingham 
+                    Ford Willingham
                   </h2>
                   <p class="text-gray-500">Product Manager</p>
                 </div>
